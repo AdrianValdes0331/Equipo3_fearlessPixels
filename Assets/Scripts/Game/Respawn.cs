76ecc0115185c359e333a88c33f03667de151674
Spike0331 <pinguinoadrian@gmail.com>
2022-10-11 11:33:27 -0500

    Lives prototipe

--- Assets/Scripts/Game/Respawn.cs
+++ Assets/Scripts/Game/Respawn.cs
@@ -8,14 +8,22 @@ public class Respawn : MonoBehaviour
     [SerializeField] public GameObject player;
     [SerializeField] public Transform spawnPoint;
     [SerializeField] float spawnValue;
+    public int lives = 2;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.position.y < -8)
+        if (((player.transform.position.y < -8 || player.transform.position.y > 20) || (player.transform.position.x < -20 || player.transform.position.x > 20)) && lives > 0)
         {
             RespawnPoint();
+            lives--;
+            Debug.Log(lives);
+        }
+        if (lives <= 0)
+        {
+            Debug.Log("asdf");
+            Destroy(transform.parent.gameObject);
         }
     }
 
