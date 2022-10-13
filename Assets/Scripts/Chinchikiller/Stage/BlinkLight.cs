38cb2dab7f3e3554b1fd211b729d208a1d3867a8
Spike0331 <pinguinoadrian@gmail.com>
2022-10-12 22:47:28 -0500

    Combined Aarons improvements

---
+++ Assets/Scripts/Chinchikiller/Stage/BlinkLight.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkLight : MonoBehaviour
+{
+    Light light;
+    bool timerOn = false;
+    public float waitSeconds;
+    public bool produceSound;
+    public AudioSource blinkLightSound;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (!timerOn)
+        {
+            StartCoroutine(blinkLight());
+        }
+    }
+
+    IEnumerator blinkLight()
+    {
+        timerOn = true;
+        yield return new WaitForSeconds(waitSeconds);
+        if (produceSound)
+        {
+            blinkLightSound.Play();
+        }
+        light.enabled = false;
+        yield return new WaitForSeconds(0.4f);
+        light.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        light.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        light.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        light.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        light.enabled = true;
+        timerOn = false;
+    }
+}
