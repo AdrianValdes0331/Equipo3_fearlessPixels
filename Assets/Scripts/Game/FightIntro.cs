1476ae576834edeb08f03d83fcca1072c8d5063e
AaronCG01 <A01730451@itesm.mx>
2022-11-01 12:25:40 -0600

    FightIntro&CameraFixes
    FightIntro images changed and camera fixes for Stralley stage.

--- Assets/Scripts/Game/FightIntro.cs
+++ Assets/Scripts/Game/FightIntro.cs
@@ -7,9 +7,12 @@ using UnityEngine.InputSystem;
 public class FightIntro : MonoBehaviour
 {
     RawImage ready, fight;
-    List<GameObject> Players = new List<GameObject>();
+    List<GameObject> Drivers = new List<GameObject>();
     Color fadedTextColor = new Color(1f, 1f, 1f, 0f);
     Color normalTextColor = new Color(1f, 1f, 1f, 1f);
+    bool readyEnded = false;
+    bool fightEnded = false;
+    bool introEnded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +27,11 @@ public class FightIntro : MonoBehaviour
 
     IEnumerator WaitForPlayers()
     {
-        yield return new WaitForSeconds(0.21f);
-        Players.AddRange(GameObject.FindGameObjectsWithTag("Player"));
-        for (int i = 0; i < Players.Count; i++)
+        yield return new WaitForSeconds(0.205f);
+        Drivers.AddRange(GameObject.FindGameObjectsWithTag("Driver"));
+        for (int i = 0; i < Drivers.Count; i++)
         {
-            PlayerInput currentPlayerInput = Players[i].GetComponent<PlayerInput>();
+            PlayerInput currentPlayerInput = Drivers[i].GetComponent<PlayerInput>();
             if (currentPlayerInput)
             {
                 currentPlayerInput.enabled = false;
@@ -36,38 +39,34 @@ public class FightIntro : MonoBehaviour
         }
     }
 
+    IEnumerator FadeText(RawImage textImage, float normalDuration, float fastDuration, int numberOfTimes, bool ended)
+    {
+        for (int i = 0; i < numberOfTimes; i ++)
+        {
+            textImage.color = normalTextColor;
+            yield return new WaitForSeconds(normalDuration);
+            textImage.color = fadedTextColor;
+            yield return new WaitForSeconds(fastDuration);
+        }
+        ended = true;
+    }
+
     IEnumerator PlayIntro()
     {
+        float normalReadyDuration = 0.5f;
+        float fastReadyDuration = 0.1f;
+        float normalFightDuration = 0.25f;
+        float fastFightDuration = 0.075f;
+        int readyTimes = 4;
+        int fightTimes = 5;
         yield return new WaitForSeconds(3f);
-        ready.color = normalTextColor;
-        yield return new WaitForSeconds(0.5f);
-        ready.color = fadedTextColor;
-        yield return new WaitForSeconds(0.15f);
-        ready.color = normalTextColor;
-        yield return new WaitForSeconds(0.5f);
-        ready.color = fadedTextColor;
-        yield return new WaitForSeconds(0.15f);
-        ready.color = normalTextColor;
-        yield return new WaitForSeconds(0.5f);
-        ready.color = fadedTextColor;
-        yield return new WaitForSeconds(0.15f);
-        ready.color = normalTextColor;
-        yield return new WaitForSeconds(0.5f);
-        ready.color = fadedTextColor;
-        fight.color = normalTextColor;
-        yield return new WaitForSeconds(0.25f);
-        fight.color = fadedTextColor;
-        yield return new WaitForSeconds(0.05f);
-        fight.color = normalTextColor;
-        yield return new WaitForSeconds(0.25f);
-        fight.color = fadedTextColor;
-        yield return new WaitForSeconds(0.05f);
-        fight.color = normalTextColor;
-        yield return new WaitForSeconds(0.25f);
-        fight.color = fadedTextColor;
-        for (int i = 0; i < Players.Count; i++)
+        StartCoroutine(FadeText(ready, normalReadyDuration, fastReadyDuration, readyTimes, readyEnded));
+        yield return new WaitForSeconds(readyTimes * (normalReadyDuration + fastReadyDuration) + 0.1f);
+        StartCoroutine(FadeText(fight, normalFightDuration, fastFightDuration, fightTimes, fightEnded));
+        yield return new WaitForSeconds(fightTimes * (normalFightDuration + fastFightDuration) + 0.1f);
+        for (int i = 0; i < Drivers.Count; i++)
         {
-            PlayerInput currentPlayerInput = Players[i].GetComponent<PlayerInput>();
+            PlayerInput currentPlayerInput = Drivers[i].GetComponent<PlayerInput>();
             if (currentPlayerInput)
             {
                 currentPlayerInput.enabled = true;
