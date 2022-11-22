55d0726dc61fbc98780ab693af139d7cb4acb8ac
archspit <82983895+archspit@users.noreply.github.com>
2022-11-22 09:19:55 -0600

    commit

--- Assets/Scripts/Multiplayer/NetworkManager.cs
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -28,6 +28,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Connecting...");
         PhotonNetwork.ConnectUsingSettings();
     }
 
