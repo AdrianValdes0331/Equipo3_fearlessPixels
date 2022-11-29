8293bb25a34dce165481c97e0279a1ac3c230d2a
Spike0331 <pinguinoadrian@gmail.com>
2022-11-29 11:43:44 -0600

    Dummy kick

--- Assets/Scripts/Attacks/DummyKick.cs
+++ Assets/Scripts/Attacks/DummyKick.cs
@@ -7,7 +7,7 @@ using System;
 public class DummyKick : MonoBehaviour, IHitboxResponder
 {     
     [SerializeField] private float dmg;
-    [SerializeField] private Hitbox hitbox;
+    [SerializeField] private NHitbox hitbox;
     [SerializeField] private int force;
     [SerializeField] private int angle;
     private bool uHitbox;
@@ -16,6 +16,23 @@ public class DummyKick : MonoBehaviour, IHitboxResponder
     {
         uHitbox = true;       
         hitbox.setResponder(this);
+
+        //pSize = System.Math.Abs(player.transform.localScale.x);
+        //queued = false;
+        //done = false;
+        //transform = player.transform;
+        hitbox.transform = transform;
+        MonoBehaviour.print("Entering Neutral");
+        //player.SetAnimatorTrigger(PlayerController.AnimStates.Neutral);
+        hitbox.Start();
+        hitbox.setResponder(this);
+        hitbox.openCollissionCheck();
+
+        //g.sz = hitbox.sz;
+        //g.isSphere = hitbox.isSphere;
+        //g.radius = hitbox.radius;
+        //g.pos = hitbox.pos;
+        //player.StartCoroutine(Active(player, activeTime));
     }
 
     void Update()
@@ -33,11 +50,11 @@ public class DummyKick : MonoBehaviour, IHitboxResponder
     {
 
         if (collider.transform.parent.transform.parent == transform.parent) { return; }
-        Hurtbox hurtbox = collider.GetComponent<Hurtbox>();
+        NHurtbox hurtbox = collider.GetComponent<NHurtbox>();
         if (hurtbox != null)
         {
-            BangLvl bang = transform.parent.GetComponent<BangLvl>();
-            bang.bangUpdate(dmg, true);
+            //BangLvl bang = transform.parent.GetComponent<BangLvl>();
+            //bang.bangUpdate(dmg, true);
             Debug.Log("Hit player");
             hurtbox.getHitBy(dmg, force, angle, transform.position.x);
         }
